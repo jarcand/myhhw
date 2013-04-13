9d1dc0bb04ea38b8e8f542babba0bfc56a7ea7aa
Jeffrey Arcand <jeffrey.arcand@ariselab.ca>
2013-04-13 01:59:49 -0400

    Created a tailer for the HHW log files, and a data struct to store details about a ride.

---
+++ src/ca/ariselab/myhhw/LogTailer.java
@@ -0,0 +1,89 @@
+/*
+ * Project:     MyRobots.com integration for ARISE Human Hamster Wheel 
+ * Authors:     Jeffrey Arcand <jeffrey.arcand@ariselab.ca>
+ * File:        ca/ariselab/myhhw/LogTailer.java
+ * Date:        Sat 2013-04-13
+ * Copyright:   Copyright (c) 2013 by Jeffrey Arcand.  All rights reserved.
+ * License:     GNU GPL v3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package ca.ariselab.myhhw;
+
+import java.io.IOException;
+
+public class LogTailer extends Tailer {
+	
+	private String rideStarted = null;
+	
+	/**
+	 * Create a new tailer for the specified HHW log file.
+	 * @param filename The file name of the log file to tail.
+	 */
+	public LogTailer(String filename) throws IOException {
+		super(filename);
+		startTailing();
+	}
+	
+	/**
+	 * Process the provided text line.
+	 * @param no The line number.
+	 * @param line The line of text to process.
+	 * @return Whether or not to continue tailing the file.
+	 */
+	public boolean processLine(int no, String line) {
+		
+		// Check if a ride is starting
+		if (line.indexOf("M_RIDE - New ride started") > 0) {
+			
+			// Save the line for when the ride ends
+			rideStarted = line;
+			
+		// Check if a ride is ending
+		} else if (line.indexOf("M_RIDE - Ride ended: ") > 0
+		  || line.indexOf(
+		    "M_FSM - Changing from state RUNNING to SAFETY_FAIL") > 0) {
+			
+			try {
+				// Create the ride struct
+				Ride r = new Ride(rideStarted, line);
+				
+				// TODO: Temp: print out the ride
+				System.out.println(r);
+				
+			} catch (IllegalArgumentException e) {
+				
+				// If there is a parsing error, print a warning
+				System.out.println("WARN: Skipping a ride " 
+				  + "because of invalid data (" + rideStarted
+				  + " // " + line + "): " + e.getMessage());
+			}
+			rideStarted = null;
+		}
+		return true;
+	}
+	
+	/**
+	 * Stand-alone test of the class.
+	 */
+	public static void main(String[] args) throws IOException {
+		
+		new LogTailer(args[0]);
+		
+		System.out.println("After start tailing.");
+	}
+}
+
---
+++ src/ca/ariselab/myhhw/Ride.java
@@ -0,0 +1,112 @@
+/*
+ * Project:     MyRobots.com integration for ARISE Human Hamster Wheel 
+ * Authors:     Jeffrey Arcand <jeffrey.arcand@ariselab.ca>
+ * File:        ca/ariselab/myhhw/Ride.java
+ * Date:        Sat 2013-04-13
+ * Copyright:   Copyright (c) 2013 by Jeffrey Arcand.  All rights reserved.
+ * License:     GNU GPL v3
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ */
+
+package ca.ariselab.myhhw;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Ride {
+	
+	private static SimpleDateFormat sdf
+	  = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+	
+	private Date rideStarted;
+	private Date rideStopped;
+	private int duration;
+	private boolean tesla = false;
+	private float calories = 0;
+		
+	/**
+	 * Create a new Ride data structure from the provided lines.
+	 * @param startedLine The line describing when the ride started.
+	 * @param stoppedLine The line describing when the ride ended.
+	 */
+	public Ride(String startedLine, String stoppedLine)
+	  throws IllegalArgumentException {
+		
+		// Parse the started timestamp
+		try {
+			rideStarted = sdf.parse(startedLine.substring(0, 19));
+		} catch (ParseException e) {
+			throw new IllegalArgumentException(
+			  "Could not parse timestamp in started line: "
+			   + startedLine.substring(0, 19));
+		}
+		
+		// Parse the stopped timestamp
+		try {
+			rideStopped = sdf.parse(stoppedLine.substring(0, 19));
+		} catch (ParseException e) {
+			throw new IllegalArgumentException(
+			  "Could not parse timestamp in stopped line: "
+			   + stoppedLine.substring(0, 19));
+		}
+		
+		// Calculate the duration
+		duration = (int)
+		  ((rideStopped.getTime() - rideStarted.getTime()) / 1000);
+		
+		// If the ride was completed properly, get additional details
+		if (stoppedLine.indexOf("M_RIDE - Ride ended:") > 0) {
+			
+			// Break the line into parts
+			String[] parts = stoppedLine.split(" ");
+			if (parts.length == 20) {
+			
+				// Check if there was a tesla coil strike
+				tesla = "true".equals(parts[18]);
+		
+				// Get the number of calories burnt
+				calories = Float.parseFloat(parts[14]);
+			}
+		}
+	}
+	
+	/**
+	 * Get a string representation of this Ride.
+	 */
+	public String toString() {
+		return "Ride (started: " + sdf.format(rideStarted)
+		  + ", stopped: " + sdf.format(rideStopped) + ", duration: "
+		  + duration + ", tesla: " + tesla + ", calories: " + calories
+		  + ")";
+	}
+	
+	/** @return The ride's started date/time. */
+	public Date getStarted() { return rideStarted; }
+	
+	/** @return The ride's stopped date/time. */
+	public Date getStopped() { return rideStopped; }
+	
+	/** @return The ride's duration. */
+	public int getDuration() { return duration; }
+	
+	/** @return The ride's tesla condition. */
+	public boolean getTesla() { return tesla; }
+	
+	/** @return The ride's calories burnt. */
+	public float getCalories() { return calories; }
+}
+
